275e7af1a8b191a3f669d60de32457b10cf3203a
matin <matin@DESKTOP-9QLS8AG>
2026-08-20 00:51:34 +0330

    service layer added

---
+++ Application/Interfaces/ITaskService.cs
@@ -0,0 +1,12 @@
+﻿using Model.Model;
+
+namespace Application.Interfaces;
+
+public interface ITaskService
+{
+    Task AddTask(TaskModel model);
+    Task DeleteTask(Guid id);
+    Task UpdateTask(TaskModel model);
+    Task<TaskModel> GetTaskById(Guid id); 
+    Task<IEnumerable<TaskModel>> GetAllTasks();
+}
---
+++ Application/Servises/TaskService.cs
@@ -0,0 +1,59 @@
+﻿using Application.Interfaces;
+using Model.Interfaces;
+using Model.Model;
+
+namespace Application.Servises;
+
+class TaskService : ITaskService
+{
+    private readonly IRepository<TaskModel> _taskRepository;
+    public TaskService(ITaskRepository taskRepository)
+    {
+        _taskRepository = taskRepository;
+    }
+    public async Task AddTask(TaskModel model)
+    {
+        try
+        {
+            await _taskRepository.AddAsync(model);
+            _taskRepository.SaveChanges();
+        }
+        catch (Exception ex) { throw ex; }
+    }
+
+    public async Task DeleteTask(Guid id)
+    {
+        try
+        {
+            _taskRepository.Remove(id);
+            _taskRepository.SaveChanges();
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+    }
+
+    public async Task<IEnumerable<TaskModel>> GetAllTasks()
+    {
+        return await _taskRepository.AllAsync();
+    }
+
+    public async Task<TaskModel> GetTaskById(Guid id)
+    {
+        return await _taskRepository.FindAsync(id);
+    }
+
+    public async Task UpdateTask(TaskModel model)
+    {
+        try
+        {
+            _taskRepository.Update(model);
+            _taskRepository.SaveChanges();
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+    }
+}
