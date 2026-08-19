09fc3f20c053e936a0f3ee42f209c93709787156
matin <matin@DESKTOP-9QLS8AG>
2026-08-20 00:22:53 +0330

    task repository created

---
+++ Persistence/Repository/TaskRepository.cs
@@ -0,0 +1,19 @@
+﻿using Model.Interfaces;
+using Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Persistence.Repository;
+
+internal class TaskRepository :Repository<Task>, ITaskRepository
+{
+    private readonly DataProviderContext _context;
+
+    public TaskRepository(DataProviderContext context)
+        : base(context)
+    {
+        _context = context;
+    }
+}
