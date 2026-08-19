6c565a73ce88bf45acb177da9b6c1cb527b2a406
matin <matin@DESKTOP-9QLS8AG>
2026-08-20 01:48:12 +0330

    task controller added

---
+++ WebApi/Controllers/TaskController.cs
@@ -0,0 +1,62 @@
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model.Model;
+
+namespace WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TaskController : ControllerBase
+{
+    private readonly ITaskService _taskService;
+    public TaskController(ITaskService taskService)
+    {
+        _taskService = taskService; 
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetTasks()
+    {
+        try
+        {
+            var result = await _taskService.GetAllTasks();
+            return Ok(result);
+        }
+        catch (Exception ex) { throw ex; }
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetTaskById(Guid id)
+    {
+        try
+        {
+            var result = await _taskService.GetTaskById(id);
+            return Ok(result);
+        }
+        catch (Exception ex) { throw ex; }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateTask([FromBody] TaskModel model)
+    {
+        try
+        {
+            await _taskService.AddTask(model);
+            return Ok();
+        }
+        catch (Exception ex) { throw ex; }
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteTask(Guid id)
+    {
+        try
+        {
+            await _taskService.DeleteTask(id);
+            return Ok();
+        }
+        catch (Exception ex) { throw ex; }
+    }
+
+}
