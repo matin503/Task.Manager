2e1cb4983ec490104972b1e89993f4bba8601e70
matin <matin@DESKTOP-9QLS8AG>
2026-08-19 23:53:58 +0330

    db context added to project

---
+++ Model/Model/TaskModelEFConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Model;
+
+public class TaskModelEFConfiguration : IEntityTypeConfiguration<TaskModel>
+{
+    public void Configure(EntityTypeBuilder<TaskModel> builder)
+    {
+        builder.ToTable("Tasks");
+        builder.HasKey(t => t.Id);
+        builder.Property(t => t.Title).IsRequired().HasMaxLength(100);
+        builder.Property(t => t.Description).HasMaxLength(500);
+        builder.Property(t => t.IsCompleted).IsRequired();
+        builder.Property(t => t.DueDate).IsRequired();
+    }
+}
---
+++ Persistence/Context/DataProviderContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model.Model;
+
+namespace Persistence.Context;
+
+public class DataProviderContext : DbContext
+{
+    DbSet<TaskModel> Tasks { get; set; }
+
+
+    public DataProviderContext(DbContextOptions<DataProviderContext> options) : base(options)
+    {}
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        try
+        {
+            var assembly = AppDomain.CurrentDomain.Load("Sanpad.Domain.Model");
+            modelBuilder.ApplyConfigurationsFromAssembly(assembly);
+        }
+        catch (Exception ex) { }
+    }
+}
