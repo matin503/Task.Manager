b3509197f125f64f05a8f4f38d782b4c9a569fc6
matin <matin@DESKTOP-9QLS8AG>
2026-08-20 01:49:58 +0330

    add cors policy

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,5 +1,10 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Model.Interfaces;
+using Persistence.Repository;
 using Persistence.Context;
+using Application.Interfaces;
+using Application.Servises;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +15,22 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataProviderContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped<ITaskRepository,TaskRepository>();
+builder.Services.AddScoped<ITaskService, TaskService >();
+
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+  
+    });
+});
 
 
 var app = builder.Build();
@@ -21,6 +42,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
+
+app.UseCors();
 
 app.MapControllers();
 
